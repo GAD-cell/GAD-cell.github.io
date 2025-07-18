a8060c07549b0e2a195922e2313ca115127903f1
Sinoué GAD <sinoue.gad@telecom-sudparis.eu>
2025-07-19 01:21:31 +0200

    react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 export default defineConfig({
   site: 'https://gad-cell.github.io/',
+  integrations: [tailwind(), react()]
 })
